33ff40303bcbdcf3f1b5f7fb95c1f510ed9b44cf
WissamAlami <45461544+WissamAlami@users.noreply.github.com>
2019-02-01 14:48:30 +0100

    Update Final algorithm.py

--- MOPSI/Final algorithm.py
+++ MOPSI/Final algorithm.py
@@ -1,5 +1,7 @@
 import pandas
 import os
+from gensim.models import doc2vec
+import csv
 
 ##Import the data
 
@@ -11,15 +13,15 @@ id=data.values[:,0]
 product_names=data.values[:,1]
 product_descriptions=data.values[:,2]
 dsc_image_urls=data.values[:,3]
+categories=data.values[:,4]
 data2 = pandas.read_csv("data2_processed.csv")
 d2v_model = doc2vec.Doc2Vec.load("d2v_model")
 
 ##Recategorize some misclassified products
 
-categories=data.values[:,4]
-categories[35396]='Root Category, Gaming, Playstation, PlayStation 4, Consoles'
-categories[372836]='Root Category, Gaming, Playstation, PlayStation 4, Consoles'
-categories[391207]='Root Category, Gaming, Playstation, PlayStation 4, Consoles'
+L=[35396,372836,391207,520889,256741,590465,898088]
+for i in L:
+  categories[i]='Root Category, Gaming, Playstation, PlayStation 4, Consoles'
 
 ##Functions
 
@@ -34,8 +36,8 @@ def newindex(i):
   for k in range(len(data2)):
     if id[i]==data2.loc[k,'ID']:
       return k
-      
-def recommandations(product):
+
+def ps4_recommandations(product):
   c=[categories[product]]
   for i in range(len(product_names)):
     if categories[product].split(',')[0:-1]==categories[i].split(',')[0:-1] and categories[i] not in c:
@@ -45,7 +47,7 @@ def recommandations(product):
   for i in range(len(product_names)):
     if product_names[i]!=product_names[product] and str(product_descriptions[i])!='nan' and str(dsc_image_urls[i])!='nan' and c[0] in categories[i]:
       d=1-similarity_test.evaluateSimilarity(product,i,data)
-      d+=descriptionDistance(newindex(product),newindex(i),data2)
+      #d+=descriptionDistance(newindex(product),newindex(i),data2)
       distances.append((d,i))
   distances=sorted(distances)
   seuil=distances[3*len(distances)//4][0]
@@ -54,9 +56,9 @@ def recommandations(product):
   for k in range(1,len(c)):
     distances=[]
     for i in range(len(product_names)):
-      if product_names[i]!=product_names[product] and str(product_descriptions[i])!='nan' and str(dsc_image_urls[i])!='nan' and c[k] in categories[i]:
+      if product_names[i]!=product_names[product] and str(product_descriptions[i])!='nan' and str(dsc_image_urls[i])!='nan' and c[k] in categories[i] and 'Playstation 2' not in product_names[i] and 'PS2' not in product_names[i] and 'PlayStation 2' not in product_names[i] and 'Ps2' not in product_names[i] and 'PS 2' not in product_names[i] and 'Ps 2' not in product_names[i]:
         d=1-similarity_test.evaluateSimilarity(product,i,data)
-        d+=descriptionDistance(newindex(product),newindex(i),data2)
+        #d+=descriptionDistance(newindex(product),newindex(i),data2)
         if d>seuil:
           distances.append((d,i))
     distances=sorted(distances)
@@ -67,11 +69,11 @@ def recommandations(product):
   
 ##Main
 
-with open(path1+r'ps4.csv', mode='w', newline="") as csvfile:
+with open(r'ps4.csv', mode='w', newline="") as csvfile:
     r = csv.writer(csvfile, delimiter=',', quotechar='"', escapechar=' ', quoting=csv.QUOTE_NONE)
     for i in range(len(product_names)):
       if categories[i]=='Root Category, Gaming, Playstation, PlayStation 4, Consoles' and str(product_descriptions[i])!='nan':
-        rec=recommandations(i)
+        rec=ps4_recommandations(i)
         if len(rec[1].split(' '))==6:
           print(i)
           r.writerow([id[i]]+rec)
