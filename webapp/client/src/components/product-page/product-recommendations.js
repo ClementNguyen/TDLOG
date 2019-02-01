eaa516ab13e3182948ba9269f66e42a6b853d458
ClementNguyen <44974718+ClementNguyen@users.noreply.github.com>
2019-02-01 19:06:57 +0100

    flask api added

---
+++ webapp/client/src/components/product-page/product-recommendations.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react';
+import SmallProduct from './small-product';
+
+class ProductRecommendations extends Component {
+    constructor(props) {
+        super();
+        this.state = {
+            id: props.id,
+            similar: [],
+            complementary: []
+        }
+    }
+
+    async componentDidMount() {
+        let recommendations_res = []
+        const response = await fetch('http://localhost:4000/recommendations', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': '*/*',
+            },
+            body: JSON.stringify({ ID: this.state.id }),
+        });
+        const recommendations = await response.json();   
+        if (recommendations[0] !== null) {
+            recommendations_res = [recommendations[0],recommendations[1]];
+        }     
+        else {
+            const response2 = await fetch('http://localhost:5000/'+this.state.id, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': '*/*',
+                },
+            });
+            const recommendations2 = await response2.json();
+            const response_similar = await fetch('http://localhost:4000/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': '*/*',
+                },
+                body: JSON.stringify({ post: recommendations2[0], post_length: -2 }),
+            });
+            const res_similar = await response_similar.json();
+            const response_complementary = await fetch('http://localhost:4000/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': '*/*',
+                },
+                body: JSON.stringify({ post: recommendations2[1], post_length: -2 }),
+            });
+            const res_complementary = await response_complementary.json();
+            recommendations_res = [res_similar, res_complementary]
+        }
+        let similar = []
+        let complementary = []
+        for (let i = 0; i < recommendations_res[0].length; i++) {
+            similar.push(<SmallProduct product={recommendations_res[0][i]} />)
+        }
+        for (let i = 0; i < recommendations_res[1].length; i++) {
+            complementary.push(<SmallProduct product={recommendations_res[1][i]} />)
+        }
+        this.setState({
+            similar: similar,
+            complementary: complementary
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <div className='grid-container'>
+                    <h1>Similar products</h1>
+                    <div className='grid'>
+                        {this.state.similar}
+                    </div>
+                </div>
+                <div className='grid-container'>
+                    <h1>Complementary products</h1>
+                    <div className='grid'>
+                        {this.state.complementary}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default ProductRecommendations;
---
+++ webapp/recommendations_api.py
@@ -0,0 +1,64 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
+import os
+import pandas as pd
+
+path = "C:\\Users\\quit7\\Desktop\\MOPSI\\code"
+os.chdir(path)
+
+def strToList(x):
+    """
+    Transform "['a', 'b']" into ['a', 'b']
+    """
+    if type(x)==str:
+        return x[2:-2].split("', '")
+
+data = pd.read_csv(".\\data\\data2_processed.csv")
+data = data.drop(['SmallImage'], axis=1)
+data.Name = data.Name.apply(strToList)
+data.Description = data.Description.apply(strToList)
+#data.Categories = data.Categories.apply(strToList)
+
+
+def findRecommendations(id):
+    product_cat_str = data[data['ID']==id]['Categories'].values[0]
+    #product_cat_str = data[data['ID']=='WA442EL0A105ZNAFAMZ']['Categories'].values[0]
+    similar = data[data['Categories']==product_cat_str].sample(5)
+    similar = similar['ID'].values
+    
+    product_cat_list = strToList(product_cat_str)
+    product_cat_list = product_cat_list[:-1]
+    complementary_cat = ', '.join("'{0}'".format(cat) for cat in product_cat_list)
+    complementary = data[data['Categories'].str.contains(complementary_cat)]
+    complementary = complementary[complementary['Categories']!=product_cat_str].sample(5)
+    complementary = complementary['ID'].values
+    
+    res = [[],[]]
+    for i in range(len(similar)):
+        res[0].append({'ID': similar[i]})
+    for i in range(len(complementary)):
+        res[1].append({'ID': complementary[i]})
+    return res
+    
+
+#set FLASK_APP=recommendations.py
+#flask run
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+app.config['SECRET_KEY'] = 'secret_key'
+
+@app.route("/")
+def test1():
+    return 'test'
+
+@app.route("/<id>", methods = ['POST'])
+def test(id):
+    if (request.method == 'POST'):
+        return jsonify(findRecommendations(id))
+        
+if __name__ == '__main__':
+    
+    app.run(host=None,port=5000)
